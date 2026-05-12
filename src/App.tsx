b8a12fae71369d2108ce0440f81c6a876ca09405
Dmytro <demolin9511@gmail.com>
2026-05-12 13:49:18 +0200

    initial create

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50 text-slate-900">
+      <h1 className="p-6 text-2x1 font-semibold">Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
