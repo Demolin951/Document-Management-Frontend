8073ae01f74f64a8cfbf9c5e5e481415af72b96c
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-05-29 14:17:04 +0200

    Connect versions page to backend types

--- src/features/versions/types/documentVersionTypes.ts
+++ src/features/versions/types/documentVersionTypes.ts
@@ -1,5 +1,5 @@
 import type { DocumentListItem } from "../../documents/types/documentTypes";
- 
+
 export type DocumentVersionListItem = {
   id: number;
   documentId: number;
@@ -7,39 +7,57 @@ export type DocumentVersionListItem = {
   uploadedBy: string;
   uploadedAtUtc: string;
 };
- 
+
+export type DocumentVersionApiResponse = {
+  id?: number;
+  Id?: number;
+  documentId?: number;
+  DocumentId?: number;
+  versionNumber?: number;
+  VersionNumber?: number;
+  uploadedBy?: string;
+  UploadedBy?: string;
+  uploadedAtUtc?: string;
+  UploadedAtUtc?: string;
+};
+
 export type VersionDocumentRow = DocumentListItem;
- 
+
 export type VersionHistoryModalProps = {
   isOpen: boolean;
   document: VersionDocumentRow | null;
   versions: DocumentVersionListItem[];
+  isLoadingVersions: boolean;
+  versionsErrorMessage: string | null;
   onClose: () => void;
   onDownloadVersion: (version: DocumentVersionListItem) => void;
 };
- 
+
 export type AvailableDocumentsVersionTableProps = {
   documents: VersionDocumentRow[];
   onDownloadLatest: (document: VersionDocumentRow) => void;
   onOpenVersionHistory: (document: VersionDocumentRow) => void;
 };
- 
+
 export type VersionDocumentRowProps = {
   document: VersionDocumentRow;
   onDownloadLatest: (document: VersionDocumentRow) => void;
   onOpenVersionHistory: (document: VersionDocumentRow) => void;
 };
- 
+
 export type VersionHistoryRowProps = {
   version: DocumentVersionListItem;
   onDownloadVersion: (version: DocumentVersionListItem) => void;
 };
- 
-export type UseMockDocumentVersionsResult = {
+
+export type UseDocumentVersionsResult = {
   selectedDocumentForVersions: VersionDocumentRow | null;
   selectedDocumentVersions: DocumentVersionListItem[];
-  openVersionHistory: (document: VersionDocumentRow) => void;
+  isLoadingVersions: boolean;
+  isVersionActionLoading: boolean;
+  versionsErrorMessage: string | null;
+  openVersionHistory: (document: VersionDocumentRow) => Promise<void>;
   closeVersionHistory: () => void;
-  downloadLatestVersion: (document: VersionDocumentRow) => void;
-  downloadVersion: (version: DocumentVersionListItem) => void;
+  downloadLatestVersion: (document: VersionDocumentRow) => Promise<void>;
+  downloadVersion: (version: DocumentVersionListItem) => Promise<void>;
 };
