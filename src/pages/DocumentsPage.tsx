6da8019ccfa9609cd3e960d7d956f359211fe591
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-06-03 14:57:04 +0200

    refactor: use selected user store in documents page

--- src/pages/DocumentsPage.tsx
+++ src/pages/DocumentsPage.tsx
@@ -1,17 +1,17 @@
+import { useSelectedUserStore } from "../app/store/useSelectedUserStore";
 import SectionCard from "../components/ui/SectionCard";
 import DocumentsTable from "../features/documents/components/DocumentTable";
-import { useAuthStore } from "../app/store/useAuthStore";
- 
+
 function DocumentsPage() {
-  const selectedUser = useAuthStore((state) => state.selectedUser);
- 
+  const selectedUser = useSelectedUserStore((state) => state.selectedUser);
+
   return (
-<div className="space-y-6">
-<SectionCard>
-<DocumentsTable username={selectedUser?.name} />
-</SectionCard>
-</div>
+    <div className="space-y-6">
+      <SectionCard>
+        <DocumentsTable username={selectedUser?.name} />
+      </SectionCard>
+    </div>
   );
 }
- 
+
 export default DocumentsPage;
