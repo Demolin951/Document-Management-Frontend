aeaf21b9103221e5b98c6a0a42d24872a7c495a2
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-06-15 15:49:26 +0200

    refactor: move upload topbar action into app wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,23 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-import { useSelectedUserStore } from "./store/useSelectedUserStore";
 import AppLayout from "../layouts/AppLayout";
-import UploadDocumentAction from "../features/documents/wrappers/UploadDocumentAction";
+import UploadDocumentTopbarAction from "./wrappers/UploadDocumentTopbarAction";
 import DashboardPage from "../pages/DashboardPage";
 import DocumentVersionsPage from "../pages/DocumentVersionsPage";
 import DocumentsPage from "../pages/DocumentsPage";
 import UsersPage from "../pages/UsersPage";
 
 function App() {
-  const selectedUser = useSelectedUserStore((state) => state.selectedUser);
-
   return (
     <BrowserRouter>
-      <AppLayout
-        topbarActions={
-          <UploadDocumentAction selectedUsername={selectedUser?.name} />
-        }
-      >
+      <AppLayout topbarActions={<UploadDocumentTopbarAction />}>
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<DashboardPage />} />
