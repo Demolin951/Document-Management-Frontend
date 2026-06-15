f16b2cb0f95a586be076cc7487c52498473905eb
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-06-15 11:36:29 +0200

    refactor: keep app composition behind app wrapper

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 
-import { useSelectedUserStore } from "./store/useSelectedUserStore";
-import UploadDocumentAction from "../features/documents/wrappers/UploadDocumentAction";
+import AppTopbarActions from "./components/AppTopbarActions";
 import AppLayout from "../layouts/AppLayout";
 import DashboardPage from "../pages/DashboardPage";
 import DocumentVersionsPage from "../pages/DocumentVersionsPage";
@@ -9,13 +8,9 @@ import DocumentsPage from "../pages/DocumentsPage";
 import UsersPage from "../pages/UsersPage";
 
 function App() {
-  const selectedUser = useSelectedUserStore((state) => state.selectedUser);
-
   return (
     <BrowserRouter>
-      <AppLayout
-        topbarActions={<UploadDocumentAction selectedUsername={selectedUser?.name} />}
-      >
+      <AppLayout topbarActions={<AppTopbarActions />}>
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<DashboardPage />} />
