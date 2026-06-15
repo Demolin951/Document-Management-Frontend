1d417a6139c096c6def52ad9d6ac34d216c81d3f
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-06-15 10:43:29 +0200

    delete old helper file

--- src/components/layout/utils/getTopbarTitle.ts
+++
@@ -1,7 +0,0 @@
-import { topbarTitleConfig } from "../configs/topbarTitleConfig";
- 
-export function getTopbarTitle(pathname: string): string {
-  const matchedRoute = topbarTitleConfig.find((item) => item.path === pathname);
- 
-  return matchedRoute?.title ?? "Document Management";
-}
