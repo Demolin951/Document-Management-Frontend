76611d7a43db9439fd2a77a22ad4b5cf0ec09158
Demo_link <149876091+Demolin951@users.noreply.github.com>
2026-06-15 11:40:40 +0200

    refactor: use shared ui and component type in version history modal

--- src/features/versions/components/VersionHistoryModal.tsx
+++ src/features/versions/components/VersionHistoryModal.tsx
@@ -1,10 +1,10 @@
 import { FileText } from "lucide-react";
 
-import DataTable from "../../../components/ui/DataTable";
-import EmptyDataTableState from "../../../components/ui/EmptyDataTableState";
-import Modal from "../../../components/ui/Modal";
+import DataTable from "../../../shared/components/ui/DataTable";
+import EmptyDataTableState from "../../../shared/components/ui/DataTableEmptyState";
+import Modal from "../../../shared/components/ui/Modal";
 import { versionHistoryTableColumns } from "../config/documentVersionsConfig";
-import type { VersionHistoryModalProps } from "../types/documentVersionTypes";
+import type { VersionHistoryModalProps } from "../types/documentVersionComponentTypes";
 
 import VersionHistoryRow from "./VersionHistoryRow";
 
